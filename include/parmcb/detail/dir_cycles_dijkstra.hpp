828e6f823e3c5b3c19c321ac5aec7bbe270c0a96
Dimitrios Michail <dimitrios.michail@gmail.com>
2023-06-05 13:20:44 +0300

    Added undirected dijkstra placeholder for second sp

--- include/parmcb/detail/dir_cycles_dijkstra.hpp
+++ include/parmcb/detail/dir_cycles_dijkstra.hpp
@@ -53,7 +53,6 @@ private:
 
     SpVecFP<P> find_shortest_non_zero_cycle_mod_p(const SpVecFP<P>& support) {
     	std::cout << "Computing shortest cycle with non-zero mod p with support = " << support << std::endl;
-    	boost::multiprecision::cpp_int minus_one(-1);
 
     	std::map<std::pair<Vertex,Vertex>, WeightType> first;
     	std::map<std::pair<Vertex,Vertex>, std::list<std::pair<bool,Edge>>> paths;
@@ -61,14 +60,13 @@ private:
     	std::map<std::pair<Vertex,Vertex>, P> residuals;
     	//std::map<std::pair<Vertex,Vertex>, WeightType> second;
 
-
+    	// Compute one shortest path between each pair of vertices. Use the undirected graph.
+    	// Directions are only used in order to compute residue classes.
     	VertexIt ui, uiend;
 		for (boost::tie(ui, uiend) = boost::vertices(_g); ui != uiend; ++ui) {
 			auto u = *ui;
 			std::cout << "Running shortest paths from " << u << std::endl;
 
-	    	// Compute one shortest path between each pair of vertices. Use the
-	    	// undirected graph. Directions are only used in order to compute residue classes.
 	        std::vector<WeightType> dist(boost::num_vertices(_g), (std::numeric_limits<WeightType>::max)());
 	        boost::function_property_map<
 	                parmcb::detail::VertexIndexFunctor<Graph, WeightType>,
@@ -89,6 +87,7 @@ private:
 			for (boost::tie(vi, viend) = boost::vertices(_g); vi != viend; ++vi) {
 				auto v = *vi;
 				if (v == u || !std::get<0>(pred_map[v])) {
+					// skip when self-loop or not reachable
 					continue;
 				}
 
@@ -103,25 +102,19 @@ private:
 
 				Vertex path_cur = v;
 				Vertex path_goal = u;
-				std::cout << "Building inverse path from " << path_cur << " to " << path_goal << std::endl;
 		        while (path_cur != path_goal) {
-		        	std::cout << "cur=" << path_cur << std::endl;
 		        	if (!std::get<0>(pred[path_cur])) {
-						throw new std::runtime_error("No path found!");
+						throw new std::runtime_error("Invalid path returned!");
 		        	}
 		        	Edge e = std::get<1>(pred[path_cur]);
-		        	std::size_t e_index = _forest_index(e);
-//		        	std::cout << "Found edge " << e << " with index " << e_index << std::endl;
-		        	SpVecFP<P> e_vec(_p, e_index);
-
+		        	std::size_t e_idx = _forest_index(e);
 		        	bool direction = boost::target(e, _g) == path_cur;
-//		        	std::cout << "Direction = " << direction << std::endl;
+		        	SpVecFP<P> e_vec(_p, e_idx);
 		        	if (!direction) {
-		        		path_vec += e_vec * minus_one;
+		        		path_vec += -e_vec;
 		        	} else {
 		        		path_vec += e_vec;
 		        	}
-//		        	std::cout << "Current path vec = " << path_vec << std::endl;
 		        	path.push_front(std::make_pair(direction, e));
 
 		        	if (direction) {
@@ -148,14 +141,43 @@ private:
 				});
 				std::cout << std::endl;
 				std::cout << "Vec path p=" << path_vec << std::endl;
-
-
 			}
-
 		}
 
-    	// TODO:
+		// Now find a second path for each pair of vertices which has a different residue class
+		// from the first path.
+		for (boost::tie(ui, uiend) = boost::vertices(_g); ui != uiend; ++ui) {
+			auto u = *ui;
+			std::cout << "Running modified shortest paths from " << u << std::endl;
 
+	        std::vector<WeightType> dist(boost::num_vertices(_g), (std::numeric_limits<WeightType>::max)());
+	        boost::function_property_map<
+	                parmcb::detail::VertexIndexFunctor<Graph, WeightType>,
+	                Vertex, WeightType&> dist_map(
+	                parmcb::detail::VertexIndexFunctor<Graph, WeightType>(dist,
+	                        _index_map));
+	    	std::vector<std::tuple<bool, Edge>> pred(boost::num_vertices(_g), std::make_tuple(false, Edge()));
+	        boost::function_property_map<
+	                parmcb::detail::VertexIndexFunctor<Graph,
+	                        std::tuple<bool, Edge>>, Vertex,
+	                std::tuple<bool, Edge>&> pred_map(
+	                parmcb::detail::VertexIndexFunctor<Graph,
+	                        std::tuple<bool, Edge> >(pred, _index_map));
+
+	        second_sp_dijkstra(_g, _weight_map, u, dist_map, pred_map);
+
+//			VertexIt vi, viend;
+//			for (boost::tie(vi, viend) = boost::vertices(_g); vi != viend; ++vi) {
+//				auto v = *vi;
+//				if (v == u || !std::get<0>(pred_map[v])) {
+//					// skip when self-loop or not reachable
+//					continue;
+//				}
+//
+//				// TODO: store final s-vu result
+//			}
+
+		}
 
 
     	// TODO
@@ -180,6 +202,84 @@ private:
 
     	// TODO
 
+    }
+
+    /**
+     * Compute the second shortest path.
+     *
+     * TODO: rewrite me to work correctly
+     */
+    template<class DistanceMap, class PredecessorMap>
+    void second_sp_dijkstra(const Graph &g, const WeightMap &weight_map,
+            const typename boost::graph_traits<Graph>::vertex_descriptor &s, DistanceMap &dist_map,
+            PredecessorMap &pred_map) {
+
+        typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
+        typedef typename boost::property_map<Graph, boost::vertex_index_t>::type VertexIndexMapType;
+        typedef typename boost::graph_traits<Graph>::edge_descriptor Edge;
+        typedef typename boost::property_traits<WeightMap>::value_type WeightType;
+        typedef typename boost::property_traits<DistanceMap>::value_type DistanceType;
+        typedef boost::d_ary_heap_indirect<Vertex, 4,
+                boost::function_property_map<parmcb::detail::VertexIndexFunctor<Graph, std::size_t>, Vertex,
+                        std::size_t&>, DistanceMap, std::less<DistanceType>> VertexQueue;
+
+        typedef typename boost::graph_traits<Graph>::directed_category DirectedCat;
+        bool is_directed = boost::detail::is_directed(DirectedCat());
+        if (!is_directed) {
+            throw new std::runtime_error("Graph is not directed.");
+        }
+
+        std::less<DistanceType> compare;
+        parmcb::detail::closed_plus<DistanceType> combine = parmcb::detail::closed_plus<DistanceType>();
+
+        const VertexIndexMapType &index_map = boost::get(boost::vertex_index, g);
+        std::vector<std::size_t> index_in_heap(boost::num_vertices(g));
+        boost::function_property_map<parmcb::detail::VertexIndexFunctor<Graph, std::size_t>, Vertex, std::size_t&> index_in_heap_map(
+                parmcb::detail::VertexIndexFunctor<Graph, std::size_t>(index_in_heap, index_map));
+
+        VertexQueue queue(dist_map, index_in_heap_map, compare);
+
+        boost::put(dist_map, s, DistanceType());
+        boost::put(pred_map, s, std::make_tuple(false, Edge()));
+        queue.push(s);
+
+        while (!queue.empty()) {
+            Vertex u = queue.top();
+            queue.pop();
+            DistanceType d_u = boost::get(dist_map, u);
+
+            auto eiOutRange = boost::out_edges(u, g);
+            auto eiInRange = boost::in_edges(u, g);
+            auto range = boost::join(boost::iterator_range<decltype(eiOutRange.first)>(eiOutRange.first, eiOutRange.second), boost::iterator_range<decltype(eiInRange.first)>(eiInRange.first, eiInRange.second));
+
+            for (const auto &e: range) {
+                auto w = boost::target(e, g);
+                if (w == u) {
+                    w = boost::source(e, g);
+                }
+                if (w == u) {
+                    // self-loop
+                    continue;
+                }
+                if (w == s) {
+                    continue;
+                }
+
+                const WeightType c = combine(d_u, get(weight_map, e));
+                bool visited_w = std::get<0>(boost::get(pred_map, w));
+                if (!visited_w) {
+                    // first time found
+                    boost::put(dist_map, w, c);
+                    boost::put(pred_map, w, std::make_tuple(true, e));
+                    queue.push(w);
+                } else if (compare(c, boost::get(dist_map, w))) {
+                    // already reached
+                    boost::put(dist_map, w, c);
+                    boost::put(pred_map, w, std::make_tuple(true, e));
+                    queue.update(w);
+                }
+            }
+        }
     }
 
     const Graph &_g;
